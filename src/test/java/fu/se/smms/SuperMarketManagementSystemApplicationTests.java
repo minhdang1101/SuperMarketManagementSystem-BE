765e6110ce39cf67f1bdb06190deb1e454179ca2
Đặng Tuấn Minh <minhdang110105@gmail.com>
2026-06-24 20:51:22 +0700

    chore: initial spring boot backend project

---
+++ src/test/java/fu/se/smms/SuperMarketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.se.smms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
