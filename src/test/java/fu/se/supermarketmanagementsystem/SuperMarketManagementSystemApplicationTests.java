c39a42da585f21557301afdd5a5de1648478a6cf
Đặng Tuấn Minh <minhdang110105@gmail.com>
2026-07-07 21:30:54 +0700

    first commit

---
+++ src/test/java/fu/se/supermarketmanagementsystem/SuperMarketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.se.supermarketmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
